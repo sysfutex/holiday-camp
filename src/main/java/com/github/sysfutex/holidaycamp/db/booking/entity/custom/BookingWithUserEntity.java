5fa57ce4bc5ff1aed1a11cd30e431b57d768ee47
Mikhail <sysfutex@gmail.com>
2024-06-29 22:50:46 +0700

    Add @Builder annotation to BookingWithUserEntity

--- src/main/java/com/github/sysfutex/holidaycamp/db/booking/entity/custom/BookingWithUserEntity.java
+++ src/main/java/com/github/sysfutex/holidaycamp/db/booking/entity/custom/BookingWithUserEntity.java
@@ -1,9 +1,11 @@
 package com.github.sysfutex.holidaycamp.db.booking.entity.custom;
 
 import com.github.sysfutex.holidaycamp.db.user.entity.custom.UserWithLocationEntity;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 
+@Builder
 public record BookingWithUserEntity(
         Long id,
 
