3e1e5f7d3437c48c93cf0d441a6f1a3ae9a89414
Mikhail <sysfutex@gmail.com>
2024-06-29 12:47:08 +0700

    Initial commit

---
+++ src/main/java/com/github/sysfutex/holidaycamp/HolidayCampApplication.java
@@ -0,0 +1,12 @@
+package com.github.sysfutex.holidaycamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // TODO
+public class HolidayCampApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayCampApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/sysfutex/holidaycamp/HolidayCampApplicationTests.java
@@ -0,0 +1,11 @@
+package com.github.sysfutex.holidaycamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayCampApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
